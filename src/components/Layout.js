75f475e93c59bba8007b19261903822611fac63d
monaco89 <nick.monaco15@gmail.com>
2020-08-06 23:17:45 -0400

    fix build fail

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 import Meta from './Meta'
@@ -6,14 +6,26 @@ import Nav from './Nav'
 import Footer from './Footer'
 import { Provider as StyletronProvider } from 'styletron-react'
 import { BaseProvider, LightTheme } from 'baseui'
-import { Client as Styletron } from 'styletron-engine-atomic'
 
 import 'modern-normalize/modern-normalize.css'
 import './globalStyles.css'
 
-const engine = new Styletron()
-
 export default ({ children, meta, title }) => {
+  const [engine, setEngine] = useState(null)
+
+  useEffect(() => {
+    // Load the `styletron-engine-atomic` package dynamically.
+    // Reason: It requires use of `document`, which is not available
+    // outside the browser, so we need to wait until it successfully loads.
+    // Source: https://www.gatsbyjs.org/docs/debugging-html-builds/
+    import('styletron-engine-atomic').then(styletron => {
+      const clientEngine = new styletron.Client()
+      setEngine(clientEngine)
+    })
+  }, [])
+
+  if (!engine) return null
+
   return (
     <StaticQuery
       query={graphql`
