f63f790a9d47d49209d67c71b2bc099d105025d1
monaco89 <nick.monaco15@gmail.com>
2020-10-04 18:30:45 -0400

    about me styling

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -19,36 +19,39 @@ const AboutMe = () => (
       }
     `}
     render={(data) => (
-      <section className="section" style={{ marginTop: '1em' }}>
+      <section className="section">
         <Row>
           <Col>
             <h2 className={AboutMeStyle.title}>About Me</h2>
           </Col>
         </Row>
-        <Row>
-          <Col sm={{ size: 'auto' }} md="5" className={AboutMeStyle.img}>
-            <Img
-              fluid={data.file.childImageSharp.fluid}
-              className={AboutMeStyle.dh}
-              alt="delaney higgins"
-            />
-          </Col>
-          <Col
-            sm={{ size: 'auto', offset: 0 }}
-            md="7"
-            className={AboutMeStyle.description}
-          >
-            <p className={AboutMeStyle.bio}>
-              Hi, I’m Delaney. I Specialize in graphic & apparel design,
-              photography, and visual merchandising. I've cultivated my skills
-              with Anthropologie over the past three and a half years. You'll
-              always find me surrounded by fresh flowers and music playing in
-              the background (On tuesdays it's my dad's radio show!) Basically,
-              I try to always surround myself with beautiful things and a lot of
-              dogs.
-            </p>
-          </Col>
-        </Row>
+        <section className={AboutMeStyle.section}>
+          <Row>
+            <Col sm={{ size: 'auto' }} md="5" className={AboutMeStyle.img}>
+              <Img
+                fluid={data.file.childImageSharp.fluid}
+                className={AboutMeStyle.dh}
+                alt="delaney higgins"
+              />
+            </Col>
+            <Col
+              sm={{ size: 'auto', offset: 0 }}
+              md="7"
+              className={AboutMeStyle.description}
+            >
+              <p className={AboutMeStyle.bio}>
+                Hi, I’m Delaney. I Specialize in graphic & apparel design,
+                photography, and visual merchandising. I've cultivated my skills
+                with Anthropologie over the past three and a half years. You'll
+                always find me surrounded by fresh flowers and music playing in
+                the background (On tuesdays it's my dad's radio show!)
+                Basically, I try to always surround myself with beautiful things
+                and a lot of dogs.
+              </p>
+              <hr className={AboutMeStyle.line} />
+            </Col>
+          </Row>
+        </section>
         {/* <div className="container">{<Content source={body} />}</div> */}
       </section>
     )}
