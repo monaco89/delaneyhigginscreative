1579f1be823d0831a6922e53e38dc38690892038
monaco89 <nick.monaco15@gmail.com>
2020-10-03 22:41:38 -0400

    fix image grid

--- src/components/ImageGallery.js
+++ src/components/ImageGallery.js
@@ -1,22 +1,15 @@
 import * as React from 'react'
 import GalleryStyle from '../style/ImageGallery.module.css'
 
-const ImageGallery = ({ images, tag }) => (
-  <div className={GalleryStyle.imageGrid}>
-    {/* {images
-      ?.filter(image => image.node.tags.includes(tag))
-      .map((image, index) => (
-        <div className="image-item" key={`${index}-cl`}>
-          <img src={image.node.secure_url} alt={'no alt :('} />
-        </div>
-      ))} */}
-    {images.map((image, i) => (
-      <div lassName={GalleryStyle.imageItem} key={`${i}-cl`}>
+const ImageGallery = ({ images }) => (
+  <div className={GalleryStyle.photos}>
+    {images.map((image) => (
+      <figure key={image.public_id}>
         <img
           src={`https://res.cloudinary.com/nickmonaco/image/upload/${image.public_id}.jpg`}
           alt="style by delaney"
         />
-      </div>
+      </figure>
     ))}
   </div>
 )
