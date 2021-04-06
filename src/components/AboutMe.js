027a058c22d19c5e344108d940f7ab300fb6b79b
monaco89 <nick.monaco15@gmail.com>
2021-04-06 08:43:47 -0400

    update copy

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -26,11 +26,11 @@ const AboutMe = () => (
         </Col>
         <Col sm="12" md={{ size: '5', offset: '1' }}>
           <p className={AboutMeStyle.bio}>
-            Delaney specializes in styling, photography, and graphic design. She
-            has cultivated her skills with Anthropologie over the past three and
-            a half years excelling in Visual Merchandising. She is currently
-            conquering Interior Design at Anna Kemper Atelier. You'll always
-            find her with a camera in her hand and music playing in the
+            Delaney specializes in styling, photography, and design. She has
+            cultivated her skills with Anthropologie over the past three and a
+            half years excelling in Visual Merchandising. She is currently
+            conquering Interior Design at Shannon Claire Interiors. You'll
+            always find her with a camera in her hand and music playing in the
             background (On tuesdays it's her dad's radio show!)
           </p>
         </Col>
